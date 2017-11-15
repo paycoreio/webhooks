a9064eb23b1afb9d6de2c902f275a7461323bd90
Dmytro Dzubenko <dmytro@dzubenko.name>
2017-11-15 23:33:45 +0200

    Remove php-fpm, delete unused files add missing www-data user

--- etc/docker/deploy.sh
+++ etc/docker/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "7.1" ]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_PHP_VERSION" == "7.1" ]]; then
   composer install -o -q -n --no-dev
   docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} -e ${DOCKER_EMAIL}
 
-  if [ "$TRAVIS_BRANCH" == "master" ] ; then
+  if [[ "$TRAVIS_BRANCH" == "master" ]] ; then
     LATEST="paymaxi/webhooks:latest"
     docker build -f etc/docker/php/Dockerfile.prod -t ${LATEST} .
     docker push ${LATEST}
