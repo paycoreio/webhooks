66d4a9491d454b75384e5a1f20bb664863c63d98
Dmytro Dzubenko <dmytro@dzubenko.name>
2017-11-15 23:47:56 +0200

    Fix broken composer command

--- etc/docker/deploy.sh
+++ etc/docker/deploy.sh
@@ -2,7 +2,7 @@
 set -xe
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_PHP_VERSION" == "7.1" ]]; then
-  composer install -o -q -n --no-dev
+  composer install -o -q -n --no-dev --ignore-platform-reqs
   docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} -e ${DOCKER_EMAIL}
 
   if [[ "$TRAVIS_BRANCH" == "master" ]] ; then
