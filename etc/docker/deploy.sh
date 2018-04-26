45448a2783676aea1a24bb3fdd5e8fb475da41cb
Dmytro Dzubenko <dmytro@dzubenko.name>
2018-04-26 17:11:14 +0300

    Update deps

--- etc/docker/deploy.sh
+++ etc/docker/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_PHP_VERSION" == "7.1" ]]; then
   composer install -o -q -n --no-dev --ignore-platform-reqs
-  docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} -e ${DOCKER_EMAIL}
+  docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
   if [[ "$TRAVIS_BRANCH" == "master" ]] ; then
     LATEST="paymaxi/webhooks:latest"
